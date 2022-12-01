9c39d3902e2e06fb16eeb320610496b1b5565e58
ManganiBiagio <biagiomangani@live.it>
2022-12-01 14:15:45 +0100

    milestone 1 completed

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <script >
+ import 'bootstrap/dist/css/bootstrap.css'
+ import AppTitle from './components/AppTitle.vue';
+
+ export default {
+  components:{AppTitle}
+ }
 
 </script>
 
 <template>
+  <div class="container">
+    <AppTitle></AppTitle>
+    
+
+  </div>
  
   
 </template>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,12 @@
+<template >
+    <h1>La mia prima app con Vite!</h1>
+    
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    
+</style>
